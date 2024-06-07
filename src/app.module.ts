9c37073a36c22ef862cc2a05e77263a488e49ccc
quasi <alferezfelipe@gmail.com>
2024-06-07 10:55:18 -0500

    add brands resource template

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarModule } from './car/car.module';
+import { BrandsModule } from './brands/brands.module';
 
 @Module({
-  imports: [CarModule],
+  imports: [CarModule, BrandsModule],
   controllers: [AppController],
   providers: [AppService],
 })
